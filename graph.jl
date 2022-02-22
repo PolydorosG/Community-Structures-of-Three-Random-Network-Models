d49b217204b2cc6270aff398e6b4b8fde9778570
Polydoros Giannouris <92758972+PolydorosG@users.noreply.github.com>
2022-02-22 19:11:18 +0200

    Update graph.jl

--- graph.jl
+++ graph.jl
@@ -1,90 +1,81 @@
+using Graphs, GraphPlot, UnicodePlots, Colors, DataFrames
 include("julia-community.jl")
-
-using DataFrames, Graphs, Plots, Base
-
 import .JuliaCommunity as juliac
 
-function swaprow!(x,i,j)
-    for k in axes(x,1) # edited according to next answer
-      idata = x[i,k]
-      x[i,k] = x[j,k]
-      x[j,k] = idata
-    end
+
+
+function run_erdos(n, ne)
+    g = erdos_renyi(n, ne);
+    nodesize = [Graphs.outdegree(g, v) for v in Graphs.vertices(g)];
+    
+    return g, gplot(g, nodesize = nodesize);
+end
+
+function run_barabasi(n, k)
+    g = barabasi_albert(n, k);
+    nodesize = [Graphs.outdegree(g, v) for v in Graphs.vertices(g)];
+    return g, gplot(g, nodesize = nodesize);
 end
 
 
-#n are the number of vertices 
-n = 1000
-#k is each vertex exepected degree 
-k = 50
-#b is the probability two vertex to be conected 
-b = 0.3
+# n = 50
+
+# for i in 2:10:100
+#     g_erdos, plot_erdos = run_erdos(n,i)
+#     display(plot_erdos)
+# end
+
+# for i in 2:5:10
+#     g_barabasi, plot_barabasi = run_barabasi(n,i)
+#     display(plot_barabasi)
+# end
+
+
+n = 1000 # number of vertices
+k = 5   # vertex exepected degree 
+b = 0.1  #probability two vertex be conected 
+
 
 g = watts_strogatz(n, k, b)
-
+# g = erdos_renyi(n, 10000);
 adj_g = adjacency_matrix(g)
-
-println(adj_g)
 
 from = adj_g.rowval
 to = zeros(Int64,length(from)) 
-id = zeros(Int64,n)
-importance = zeros(Int64,length(id))
-weight = zeros(Int64,length(from))
+id = [i for i=1:n]
 
 for i in 1:n
-    id[i] = i
-    importance[i] = 1
     for j in (adj_g.colptr[i]):(adj_g.colptr[i+1]-1)
-        to[j] = i;
-        weight[j] = 1
+        to[j] = i
     end
 end
 
-println(id)
-println(from)
-println(to)
-println(importance)
-println(weight)
-display(spy(adj_g))
-
-nodes = DataFrame(id = id, 
-               label = id, 
-            importance = importance)
-network = DataFrame(from = from,
-                      to = to,
-                  weight = weight)
+nodes = DataFrame(id = id, label = id, importance = ones(Int64,n))
+network = DataFrame(from = from, to = to, weight = ones(Int64,size(from)))
 
 
-#================================================================
-create a JuliaCommunity instance.
-task_series is used to name the processing data files (e.g., data/communities-$task_series.csv) and 
-the svgs (e.g., fig/network-graph-$task_series.svg or fig/community-$(community_id)-$task_series.svg).
-================================================================#
 jc = juliac.JuliaCommunityInstance(network, nodes = nodes, node_label_field = "label", 
-                                   node_weighted = true, to_summarise_graph = false, task_series = "leiden")
+                                   node_weighted = true, is_directed = false,
+                                   to_summarise_graph = false, task_series = "leiden")
 
 #plot the entire network/graph
-# juliac.plot_network(jc ,n, line_type="straight", node_size_smoother = 0.8, edge_width_smoother = 1.2)
+juliac.plot_network(jc , line_type="straight", node_size_smoother = 0.8, edge_width_smoother = 1.2)
 
 jc.γ = 0.1
 
 juliac.discover_communities(jc)
+display(spy(adj_g))
 
-println(jc.communities)
 println(jc.memberships)
-sort!(jc.memberships,[:c])
-println(jc.memberships)
+# sort!(jc.memberships,[:c])
+
+coms = [hcat(jc.memberships.id)  hcat(jc.memberships.c)]
+coms = coms[sortperm(coms[:, 2]), :]; # sorted by communities
+
 A = Matrix(adj_g)
-# println(A)
 
-for i in 1:n
-    swaprow!(A,jc.memberships.id[i],i)
-end
-# println(A)
-# A = sparse(A,n,n)
-# display(spy(A))
-
+A = A[coms[:,1], coms[:,1]]
+display(spy(A))
 
 # for i in 1:jc.n_community
 #     juliac.plot_community(jc, i, line_type="straight")
