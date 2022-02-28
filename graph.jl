9bb16a1ae1c7fce94731ee9e14759ec0d32b331d
Polydoros Giannouris <92758972+PolydorosG@users.noreply.github.com>
2022-03-01 00:09:42 +0200

    Update graph.jl

--- graph.jl
+++ graph.jl
@@ -3,22 +3,9 @@ include("julia-community.jl")
 import .JuliaCommunity as juliac
 
 
-
-function run_erdos(n, ne)
-    g = erdos_renyi(n, ne);
-    nodesize = [Graphs.outdegree(g, v) for v in Graphs.vertices(g)];
-    
-    return g, gplot(g, nodesize = nodesize);
-end
-
-function run_barabasi(n, k)
-    g = barabasi_albert(n, k);
-    nodesize = [Graphs.outdegree(g, v) for v in Graphs.vertices(g)];
-    return g, gplot(g, nodesize = nodesize);
-end
-
-function barabasi_graph(n,k)
-    g = barabasi_albert(n, k)
+# Create an Erdos-Renyi graph and return it in form suitable for Leiden algorithm
+function erdos_graph(n, p)
+    g = erdos_renyi(n, p);
     
     adj_g = adjacency_matrix(g)
     from = adj_g.rowval
@@ -35,24 +22,8 @@ function barabasi_graph(n,k)
     return  adj_g , from , to , id; 
 end
 
-function erdos_graph(n,ne)
-    g = erdos_renyi(n, ne);
-    
-    adj_g = adjacency_matrix(g)
-    from = adj_g.rowval
-    
-    to = zeros(Int64,length(from)) 
-    id = [i for i=1:n]
 
-    for i in 1:n
-        for j in (adj_g.colptr[i]):(adj_g.colptr[i+1]-1)
-            to[j] = i
-        end
-    end
-
-    return  adj_g , from , to , id; 
-end
-
+# Create a Watts-Strogatz graph and return it in form suitable for Leiden algorithm
 function watts_strogatz_graph(n , k , b)
     g = watts_strogatz(n, k, b)
     
@@ -72,70 +43,127 @@ function watts_strogatz_graph(n , k , b)
 end
 
 
-# n = 50
+# Create a Barabasi-Albert graph and return it in form suitable for Leiden algorithm
+function barabasi_graph(n, k)
+    g = barabasi_albert(n, k)
+    
+    adj_g = adjacency_matrix(g)
+    from = adj_g.rowval
+    
+    to = zeros(Int64,length(from)) 
+    id = [i for i=1:n]
 
-# for i in 2:10:100
-#     g_erdos, plot_erdos = run_erdos(n,i)
-#     display(plot_erdos)
-# end
+    for i in 1:n
+        for j in (adj_g.colptr[i]):(adj_g.colptr[i+1]-1)
+            to[j] = i
+        end
+    end
 
-# for i in 2:5:10
-#     g_barabasi, plot_barabasi = run_barabasi(n,i)
-#     display(plot_barabasi)
-# end
+    return  adj_g , from , to , id; 
+end
+
 
 
 n = 1000 # number of vertices
-k = 5   # vertex exepected degree 
-b = 0.1  #probability two vertex be conected 
+
+# # Uncomment to produce Erdos Renyi matrices 
+# for p in 0.001:0.003:0.09
+#     local k = 5   # vertex exepected degree 
+#     local adj_g , from , to , id = erdos_graph(n , p)
 
 
-# g = watts_strogatz(n, k, b)
-# # g = erdos_renyi(n, 10000);
-# adj_g = adjacency_matrix(g)
+#     local nodes = DataFrame(id = id, label = id, importance = ones(Int64,n))
+#     local network = DataFrame(from = from, to = to, weight = ones(Int64,size(from)))
 
-# from = adj_g.rowval
-# to = zeros(Int64,length(from)) 
-# id = [i for i=1:n]
 
-# for i in 1:n
-#     for j in (adj_g.colptr[i]):(adj_g.colptr[i+1]-1)
-#         to[j] = i
+#     local jc = juliac.JuliaCommunityInstance(network, nodes = nodes, node_label_field = "label", 
+#                                     node_weighted = true, is_directed = false,
+#                                     to_summarise_graph = false, task_series = "leiden")
+
+#     #plot the entire network/graph
+#     #juliac.plot_network(jc , line_type="straight", node_size_smoother = 0.8, edge_width_smoother = 1.2)
+
+#     jc.γ = 0.1
+
+#     juliac.discover_communities(jc)
+#     println(p)
+
+#     local coms = [hcat(jc.memberships.id)  hcat(jc.memberships.c)]
+#     local coms = coms[sortperm(coms[:, 2]), :]; # sorted by communities
+
+#     local A = Matrix(adj_g)
+
+#     A = A[coms[:,1], coms[:,1]]
+#     display(spy(A, plot_title="p = " * string(p)))
+# end
+
+
+
+# Uncomment to produce Watts Strogatz matrices
+# for k in [10, 40]
+#     for b in [0.05, 0.8]
+        
+#         local adj_g , from , to , id = watts_strogatz_graph(n, k, b)
+
+#         local nodes = DataFrame(id = id, label = id, importance = ones(Int64,n))
+#         local network = DataFrame(from = from, to = to, weight = ones(Int64,size(from)))
+
+
+#         local jc = juliac.JuliaCommunityInstance(network, nodes = nodes, node_label_field = "label", 
+#                                         node_weighted = true, is_directed = false,
+#                                         to_summarise_graph = false, task_series = "leiden")
+
+#         #plot the entire network/graph
+#         #juliac.plot_network(jc , line_type="straight", node_size_smoother = 0.8, edge_width_smoother = 1.2)
+
+#         jc.γ = 0.1
+
+#         juliac.discover_communities(jc)
+#         println(b)
+
+#         local coms = [hcat(jc.memberships.id)  hcat(jc.memberships.c)]
+        
+#         # sort vertices by their community
+#         coms = coms[sortperm(coms[:, 2]), :]; 
+
+#         local A = Matrix(adj_g)
+
+#         A = A[coms[:,1], coms[:,1]]
+#         display(spy(A, plot_title = "k = " * string(k) * ", b = " * string(b) ))
 #     end
 # end
 
-# adj_g , from , to , id = barabasi_graph(n , 10)
-# adj_g , from , to , id = watts_strogatz_graph(n , k , b)
-adj_g , from , to , id = erdos_graph(n , 100)
 
 
-nodes = DataFrame(id = id, label = id, importance = ones(Int64,n))
-network = DataFrame(from = from, to = to, weight = ones(Int64,size(from)))
+# Uncomment to produce Barabasi Albert matrices
+for k in [2,8,16,36]
+    local adj_g , from , to , id = barabasi_graph(n , k)
 
 
-jc = juliac.JuliaCommunityInstance(network, nodes = nodes, node_label_field = "label", 
-                                   node_weighted = true, is_directed = false,
-                                   to_summarise_graph = false, task_series = "leiden")
+    local nodes = DataFrame(id = id, label = id, importance = ones(Int64,n))
+    local network = DataFrame(from = from, to = to, weight = ones(Int64,size(from)))
 
-#plot the entire network/graph
-juliac.plot_network(jc , line_type="straight", node_size_smoother = 0.8, edge_width_smoother = 1.2)
 
-jc.γ = 0.1
+    local jc = juliac.JuliaCommunityInstance(network, nodes = nodes, node_label_field = "label", 
+                                    node_weighted = true, is_directed = false,
+                                    to_summarise_graph = false, task_series = "leiden")
 
-juliac.discover_communities(jc)
-# display(spy(adj_g))
+    #plot the entire network/graph
+    #juliac.plot_network(jc , line_type="straight", node_size_smoother = 0.8, edge_width_smoother = 1.2)
 
-println(jc.memberships)
-# sort!(jc.memberships,[:c])
+    jc.γ = 0.1
 
-coms = [hcat(jc.memberships.id)  hcat(jc.memberships.c)]
-coms = coms[sortperm(coms[:, 2]), :]; # sorted by communities
+    juliac.discover_communities(jc)
+    println(k)
 
-A = Matrix(adj_g)
+    local coms = [hcat(jc.memberships.id)  hcat(jc.memberships.c)]
+    local coms = coms[sortperm(coms[:, 2]), :]; # sorted by communities
 
-A = A[coms[:,1], coms[:,1]]
-spy(A)
+    local A = Matrix(adj_g)
 
-# for i in 1:jc.n_community
-#     juliac.plot_community(jc, i, line_type="straight")
-# end
+    A = A[coms[:,1], coms[:,1]]
+    display(spy(A, plot_title = "k = " * string(k)))
+end
+
+
+
